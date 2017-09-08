a8f40541af1abffcba45021abef7c53360c51991
Titipat <titipatsukhvibul@gmail.com>
2017-09-08 09:56:05 +0700

    Refactor: split to separate lib file

--- index.js
+++ index.js
@@ -1,108 +1,10 @@
-const google = require("googleapis")
-const sheets = google.sheets("v4")
 const _ = require("lodash")
 const dotenv = require("dotenv").config()
-const KEY = JSON.parse(
-  Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT, "base64").toString()
-)
 const pug = require("pug")
-const SPREAD_SHEET_ID = process.env.SPREAD_SHEET_ID
-
-if (_.isEmpty(SPREAD_SHEET_ID)) {
-  throw new Error("SPREAD_SHEET_ID is required")
-}
-
-const jwtClient = new google.auth.JWT(
-  KEY.client_email,
-  null,
-  KEY.private_key,
-  ["https://www.googleapis.com/auth/spreadsheets.readonly"], // an array of auth scopes
-  null
-)
-
-const getSheet = callback => {
-  jwtClient.authorize((err, tokens) => {
-    if (err) {
-      console.error(err)
-    }
-
-    const req = {
-      spreadsheetId: SPREAD_SHEET_ID,
-      auth: jwtClient,
-      range: ["summary"]
-    }
-
-    sheets.spreadsheets.values.get(req, (err, res) => {
-      callback(err, res)
-      // if (err) {
-      //     console.error(err)
-      // }
-      // console.log(res)
-    })
-  })
-}
-
-// getSheet((err, sheet) => {
-// console.log(sheet)
-// })
-
-const getHeader = callback => {
-  getSheet((err, sheet) => {
-    // console.log(sheet)
-    callback(err, {
-      labels: sheet.values[0],
-      fullMarks: sheet.values[1]
-    })
-  })
-}
-
-const getScore = (studentId, callback) => {
-  getSheet((err, sheet) => {
-    getHeader((err, header) => {
-      const score = sheet.values
-        .filter(record => {
-          return record[1] == studentId
-        })
-        .pop()
-
-      let response = {}
-      for (const index in header.labels) {
-        response[header.labels[index]] = {
-          value: score[index]
-        }
-
-        if (header.fullMarks[index].length > 0) {
-          response[header.labels[index]].fullMark = _.toNumber(
-            header.fullMarks[index]
-          )
-          response[header.labels[index]].type = "number"
-        } else {
-          response[header.labels[index]].type = "string"
-        }
-      }
-
-      callback(err, response)
-    })
-  })
-}
+const sheet = require('./lib/sheet')
+const {getSheet, getHeader, getScore} = sheet
 
 const studentId = "580610631"
-// getScore(studentId, (err, score) => {
-//   const student = {
-//     id: score.id.value,
-//     firstname: score.firstname.value.toString("utf8"),
-//     lastname: score.lastname.value
-//   };
-//   console.log(score);
-//   const fs = require("fs");
-//   const html = pug.renderFile("template.pug", {
-//     student,
-//     score
-//   });
-//   // console.log(html)
-//   fs.writeFileSync("./output.html", html);
-// });
-
 const students = [
   {
     id: "580610631",
@@ -148,6 +50,19 @@ app.get("/myscoreboard", (req, res) => {
       // console.log(html)
       res.send(html)
     })
+  })
+})
+
+app.get('/students/:id', (req, res) => {
+  const {id} = req.params
+  getScore(id, (err, score) => {
+    if (err) {
+      console.error(err)
+      if (err.toString() === 'Error: Student not found') {
+        return res.sendStatus(404)
+      }
+    }
+    res.json(score)
   })
 })
 
